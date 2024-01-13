417438697879f6da11b83d469ea01b2242b61485
anubhavkumar5500 <156525991+anubhavkumar5500@users.noreply.github.com>
2024-01-13 21:31:54 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					"https://latestnewsinfousa.online/" + encodeURI(path as string)
+					"https://latestnewsinfousa.online/"+ encodeURI(path as string)
 				}`,
 			},
 		};
